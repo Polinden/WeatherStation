ce6566dc01988d5248659b06b6bccf0defefbd47
Polina <43998896+Polinden@users.noreply.github.com>
2020-11-15 13:29:54 +0200

    Update Matrix.ino

--- Matrix.ino
+++ Matrix.ino
@@ -3,6 +3,7 @@
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
+#include "RTClib.h"
 
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
@@ -11,14 +12,18 @@
 #define CS_PIN    7
 
 MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+RTC_DS1307 rtc;
 
 
-#define  BUF_SIZE  100
+#define BUF_SIZE 100
 char curMessage[BUF_SIZE] = { "" };
-char newMessage[BUF_SIZE] = { "Hello, Michael!" };
+char dateNow[BUF_SIZE] = { "" };
+char newMessage[BUF_SIZE] = { "" };
+char daysOfTheWeek[7][4] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 bool newMessageAvailable = true;
+bool my_turn=true;
 AltSoftSerial altser;
-int buf_step=0;
+
 
 
 void readSerial(void)
@@ -40,20 +45,37 @@ void readSerial(void)
 }
 
 
+void readTime(void)
+{     
+  DateTime now = rtc.now();
+  strcpy(dateNow,"%d.%d.%d, %s, %d:%d\0");
+  snprintf(dateNow, strlen(dateNow), now.month(), now.month(), now.year(), daysOfTheWeek[now.dayOfTheWeek()], now.hour(), now.minute());
+}
+
+
 void setup()
 {
   Serial.begin(9600);
   altser.begin(9600);
+  rtc.begin();
   P.begin();
-  P.displayText(curMessage, PA_LEFT, 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  P.displayText(curMessage, PA_LEFT, 70, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  readTime();
 }
+
+
+
 
 void loop()
 {
   if (P.displayAnimate())
   {
+    if (my_turn) P.displayText(dateNow, PA_CENTER, 100, 10, PA_SCROLL_DOWN, PA_SCROLL_UP)
+    else P.displayText(curMessage, PA_LEFT, 70, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);      
+   
     if (newMessageAvailable)
-    {
+    {      
       strcpy(curMessage, newMessage);
       newMessageAvailable = false;
     }
